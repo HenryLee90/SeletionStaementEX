07fe1caf1d4a4e5de3744d552581010d2f8d31ad
Henry Lee <hlee09151@gmail.com>
2022-04-26 09:34:00 -0500

    Init

---
+++ SelectionStatementEX/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SelectionStatementEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int faveNum = 21;
+            Console.WriteLine($"Guess my favorite Number between 1 and 30");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == faveNum)
+            {
+                Console.WriteLine($"Great job you got it!!");
+
+
+
+            }
+            else if (userInput < faveNum)
+            {
+                Console.WriteLine($"A little to low my guy, bump it up some");
+
+
+
+            }
+            else if (userInput > faveNum)
+            {
+                Console.WriteLine($"Hey man Grandma said turn it down some..");
+
+
+
+            }
+            else ()
+            {
+
+                Console.WriteLine($"Sorry this dont make sense to me?");
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }  
+
+}
